a25831ca403ab56ab11a5ed167f9a6c7d0cf218a
blessi30ng <blessingchidewu3@gmail.com>
2023-12-13 01:37:36 -0800

    yaaaayayy

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
-int main()
+/**
+ * main - main
+ * Return: 0
+ */
+
+int main(void)
 {
 	while (1)
 	{
 		display_prompt();
+
 		char input[MAX_INPUT_SIZE];
-		
+
 		if (fgets(input, sizeof(input), stdin) == NULL)
 		{
-			printf("\nExiting shell. Goodbye!\n");
+			printf("\nExiting shell. Bye!\n");
 			break;
 		}
 		input[strcspn(input, "\n")] = '\0';
 		execute_command(input);
 	}
-       return 0;
+	return (0);
 }
-
