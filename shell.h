5b33d84f9ba84d50e9e5f76ec33bc0fdde8803d8
DNM-lang <njorogedennis59@gmail.com>
2023-12-15 01:37:29 -0800

    update

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,7 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_ARG 125;
+#define MAX_INPUT_SIZE 1024
+
 void display_shell_prompt();
 void execute_shell_command(char *input);
 
