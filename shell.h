748657b5bb30e91131ea60dbcbf1321b9a6ae752
blessi30ng <blessingchidewu3@gmail.com>
2023-12-15 01:27:09 -0800

    lol

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,24 +9,11 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-#define MAX_INPUT_SIZE 1024
+#define MAX_ARG 125;
 
-extern char **environ;
-
-char *read_input(void);
-void display_prompt(void);
-void executable_command(char *input);
-void customized_access(char *path, int mode);
-void customized_chdir(char *path);
-void customized_close(int fd);
-void executable_builtin(char *command);
-void print_environment_variables(void);
-void customized_exit(void);
-void customize__exit(int status);
-char *find_executable_file(char *command);
-char *_strdup(char *s);
-int _strcmp(char *s1, char *s2);
-void executed_command(char *command);
-void pliz_interact_mode(void);
-void dont_interact_mode(FILE *file);
+int display_command(void);
+char *getpath(char **env);
+int exeCmd(char *argC[]);
+int tokenize(char *command, char *argsC[]);
+char *removedNC(char *str, int len);
 #endif
