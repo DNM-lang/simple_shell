857327fae69c9e4b6041cf51b6e4f3563c0da373
blessi30ng <blessingchidewu3@gmail.com>
2023-12-15 07:46:44 -0800

    yoooh

---
+++ path.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * look_for_path - Looks for a path
+ * @command: Command
+ * @fpath: Full path of the command
+ * @path: Path variable
+ * Return: Full path of the command
+ */
+
+char *look_for_path(char *command, char *fpath, char *path)
+{
+	unsigned int comnd_len, p_len, o_len;
+	char *path_cpy, *token;
+
+	comnd_length = _strlen(command);
+	o_len = _strlen(path);
+	path_cpy = malloc(sizeof(char) * o_len + 1);
+	if (path_cpy == NULL)
+	{
+		err_msg(3);
+		return (NULL);
+	}
+	_strcpy(path_cpy, path);
+	token = strtok(path_cpy, ":");
+	if (token == NULL)
+		token = strtok(NULL, ":");
+	while (token != NULL)
+	{
+		p_len = _strlen(token);
+		fpath = malloc(sizeof(char) * (p_len + comnd_len) + 2);
+		if (fpath == NULL)
+		{
+			err_msg(3);
+			return (NULL);
+		}
+		_strcpy(fpath, token);
+		fpath[p_len] = '/';
+		_strcpy(fpath + p_len + 1, command);
+		fpath[p_len + comnd_len + 1] = '\0';
+		if (access(fpath, X_OK) != 0)
+		{
+			free(fpath);
+			fpath = NULL;
+
+			token = strtok(NULL, ":");
+		}
+		else
+			break;
+	}
+	free(path_cpy);
+	return (fpath);
+}
